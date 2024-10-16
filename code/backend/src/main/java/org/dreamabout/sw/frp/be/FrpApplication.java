df0b1ed8fdd9e3ee171d96ebf78d7d6435772842
dvdmchl <dvd.mchl@gmail.com>
2024-10-16 18:50:55 +0200

    Add backend and frontend modules
    Introduced backend and frontend submodules in pom.xml. Implemented Spring Boot skeleton for backend with an initial application class. Updated .gitignore to exclude target directories.

---
+++ code/backend/src/main/java/org/dreamabout/sw/frp/be/FrpApplication.java
@@ -0,0 +1,12 @@
+package org.dreamabout.sw.frp.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrpApplication.class, args);
+    }
+}
